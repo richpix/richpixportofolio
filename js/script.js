efeaacb01bf0d309dd06498e953930c33186b806
richpix <pool.ricardo.1fm@gmail.com>
2026-04-29 21:09:09 -0500

    add: mejora ui para movil

--- js/script.js
+++ js/script.js
@@ -204,10 +204,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('hamburger').addEventListener('click', () => {
+      
       document.getElementById('navLinks').classList.toggle('open');
     });
     
     document.querySelectorAll('.nav-links a').forEach(a => {
+      
       a.addEventListener('click', () => document.getElementById('navLinks').classList.remove('open'));
     });
 
